60847bad1ff43aff24790e57dc1f33aa8858ac60
drewjbartlett <drewb9@gmail.com>
2018-05-24 22:56:12 -0700

    Add spacing below imports

--- src/core/mixins/init.js
+++ src/core/mixins/init.js
@@ -7,6 +7,7 @@ import { routeTypes } from '../../config';
 import { generateRoute } from '../../utils/routes';
 import { sanitizeUrl } from '../../utils/url';
 import { applyCallableRequestMethods } from './request';
+
 /**
  * Loop through the routes and set them
  *
