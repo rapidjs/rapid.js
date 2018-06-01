fedcaab7ed56a2e6fff507644333f61a502cf454
drewjbartlett <drewb9@gmail.com>
2018-06-01 08:44:20 -0700

    Break request method and suffix assertions into individual tests

--- test/core/config/request-methods.test.js
+++ test/core/config/request-methods.test.js
@@ -11,19 +11,25 @@ const anotherMock = createRapid({
 });
 
 describe('Config: Request Method Types', () => {
-  it('should overwrite default request method types with config method types', () => {
+  it('should overwrite default request method for create()', () => {
     anotherMock.create({}).then(response => {
       expect(response.requestType).toBe('put');
     });
+  });
 
+  it('should overwrite default request method for update()', () => {
     anotherMock.update(10, {}).then(response => {
       expect(response.requestType).toBe('patch');
     });
+  });
 
+  it('should overwrite default request method for delete()', () => {
     anotherMock.destroy(45).then(response => {
       expect(response.requestType).toBe('delete');
     });
+  });
 
+  it('should overwrite default request method for restore()', () => {
     anotherMock.restore(23).then(response => {
       expect(response.requestType).toBe('post');
     });
--- test/core/config/request-suffixes.test.js
+++ test/core/config/request-suffixes.test.js
@@ -11,19 +11,25 @@ const anotherMock = createRapid({
 });
 
 describe('Config: Request Suffixes', () => {
-  it('should overwrite default request suffixes with config suffixes', () => {
+  it('should overwrite default request suffixe for create()', () => {
     anotherMock.create({}).then(response => {
       expect(response.url).toBe('api/photo-album/new');
     });
+  });
 
+  it('should overwrite default request suffixe for update()', () => {
     anotherMock.update(10, {}).then(response => {
       expect(response.url).toBe('api/photo-album/10/save');
     });
+  });
 
+  it('should overwrite default request suffixe for destroy()', () => {
     anotherMock.destroy(45).then(response => {
       expect(response.url).toBe('api/photo-album/45/delete');
     });
+  });
 
+  it('should overwrite default request suffixe for restore()', () => {
     anotherMock.restore(23).then(response => {
       expect(response.url).toBe('api/photo-album/23/undelete');
     });
