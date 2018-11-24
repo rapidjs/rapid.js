d8ed5b168e9b7f502687902ac05872f9d546a94b
Drew Bartlett <drewb9@gmail.com>
2018-11-23 18:30:38 -0800

    Add tests for .default, .model, .collection

--- test/core/config/routes.test.js
+++ test/core/config/routes.test.js
@@ -46,3 +46,38 @@ describe('Routes are generated properly based off config', () => {
   });
 });
 
+describe('model, collection, and default routes', () => {
+  const trail = createRapid({
+    modelName: 'trail',
+  });
+
+  describe('.model', () => {
+    it('should set routes to the singular model route', () => {
+      trail.model.get('/foo').then(response => {
+        expect(response.url).toBe('api/trail/foo');
+      });
+    });
+  });
+
+  describe('.collection', () => {
+    it('should set routes to the plural collection route', () => {
+      trail.collection.get('/foo').then(response => {
+        expect(response.url).toBe('api/trails/foo');
+      });
+    });
+  });
+
+  describe('.default', () => {
+    const foo = createRapid({
+      modelName: 'bar',
+      defaultRoute: 'collection',
+    });
+
+    it('should set the route back to the default route set in the config', () => {
+      foo.default.get('/foo').then(response => {
+        expect(response.url).toBe('api/bars/foo');
+      });
+    });
+  });
+});
+
